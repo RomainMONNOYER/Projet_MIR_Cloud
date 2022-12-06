a0bba8d42b177fb30e1eeb2ac1be174076e554be
RomainMONNOYER <Romain.Monnoyer@student.umons.ac.be>
2022-12-06 12:29:30 +0100

    feat(mainapp): automated deploy

--- MIR/views.py
+++ MIR/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index  MA BITE.")
+    latest_question_list = "This is my question"
+    template = loader.get_template('test.html')
+    context = {
+        'latest_question_list': [latest_question_list],
+    }
+    return HttpResponse(template.render(context, request))
