f5be3ee89c2fd57d2193859fb910151cd5e23db9
RomainMONNOYER <Romain.Monnoyer@student.umons.ac.be>
2022-12-19 08:49:04 +0100

    feat(mainapp): change url base

--- app/Projet_MIR_Cloud/urls.py
+++ app/Projet_MIR_Cloud/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('MIR/', include('MIR.urls')),
+    path('', include('MIR.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
