ba720e08a9e4cdd702d54be36ba1a9ea8e64d3cd
RomainMONNOYER <Romain.Monnoyer@student.umons.ac.be>
2023-01-10 18:15:11 +0100

    feat(mainapp): add mysql db

--- app/Projet_MIR_Cloud/settings.py
+++ app/Projet_MIR_Cloud/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'Projet_MIR_Cloud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'), # DB name
+        'USER': env('DATABASE_USER'), # DB account
+        'PASSWORD': env('DATABASE_PASSWORD'), # DB account's password
+        'HOST': env('DATABASE_HOST') # DB address IP
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
